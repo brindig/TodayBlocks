3d8023e03ebf3ec9c3106d8e0a76c881e680c681
brindig <brindig@gmail.com>
2013-08-22 19:58:15 -0400

    email addresses are unique and indexed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 32 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
