d196562fd026bb75e56808686417fbe459aae3fd
brindig <brindig@gmail.com>
2013-08-23 19:46:26 -0400

    added factorygirl user show testing, skipped gravatar

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dave Chappelle"
+    email    "dave@chappelle.com"
+    password "rickjamesbitch"
+    password_confirmation "rickjamesbitch"
+  end
+end
