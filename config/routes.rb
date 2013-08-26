b95cf0ffa2485e944a6185a63bbb54646eb5c1f5
brindig <brindig@gmail.com>
2013-08-26 14:26:01 -0400

    added main, modified routes. about to unleash hell

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Sampleapp::Application.routes.draw do
   match '/help',     to: 'static_pages#help'
   match '/about',    to: 'static_pages#about'
   match '/contact',  to: 'static_pages#contact'
+  match '/main',     to: 'static_pages#main'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
